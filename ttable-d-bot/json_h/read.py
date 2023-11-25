ec7a0a12821a246b6559fbe61f3856f7a747cc23
Alexander Bürow <41566305+Cheerfulmoss@users.noreply.github.com>
2023-11-26 02:52:48 +1000

    Add files via upload

--- ttable-d-bot/json_h/read.py
+++ ttable-d-bot/json_h/read.py
@@ -1,22 +1,39 @@
 import logging
 import json
 import os
+import shutil
+from datetime import datetime
+
+from .write import JsonWriter as jw
 
 
-def clear_json(logger: logging.Logger, file_path: str):
-    with open(file_path, "w") as file:
-        json.dump({}, file)
-    logger.info(f"{os.path.basename(file_path)} cleared/reset.")
+class JsonReader:
+    def __init__(self):
+        self._default_logger = logging.getLogger(__name__)
 
+    def extract_from_json_cache(self, file_path: str,
+                                logger: logging.Logger = None):
+        logger = logger or self._default_logger
+        filename = os.path.basename(file_path)
 
-def extract_from_json(logger: logging.Logger, file_path: str):
-    try:
-        with open(file_path, "r") as file:
-            data = json.load(file)
-        return data
-    except json.JSONDecodeError as e:
-        logger.error(f"Error decoding JSON: {e}")
-    except FileNotFoundError as e:
-        logger.error(f"{os.path.basename(file_path)} DNE: {e}")
-    clear_json(logger, file_path)
-    return {}
+        try:
+            with open(file_path, "r") as file:
+                data = json.load(file)
+            return data
+        except json.JSONDecodeError as e:
+            logger.error(f"Error decoding JSON: {e}")
+        except UnicodeError as e:
+            logger.error(f"Unicode error while decoding {filename}: {e}")
+        except ValueError as e:
+            logger.error(f"Unexpected JSON structure in {filename}: {e}")
+        except FileNotFoundError as e:
+            logger.error(f"{filename} DNE: {e}")
+        except PermissionError as e:
+            logger.error(f"Permission error while accessing {filename}: {e}")
+        except IsADirectoryError as e:
+            logger.error(f"{filename} is a directory, not a file: {e}")
+        except IOError as e:
+            logger.error(f"I/O error while working with {filename}: {e}")
+
+        jw().clear_json(file_path, logger=logger)
+        return {}
---
+++ ttable-d-bot/json_h/write.py
@@ -0,0 +1,42 @@
+import logging
+import json
+import os
+import shutil
+from datetime import datetime
+
+
+class JsonWriter:
+    def __init__(self):
+        self._default_logger = logging.getLogger(__name__)
+
+    def backup_json(self, file_path: str, backup_folder: str = "backup",
+                    logger: logging.Logger = None):
+        logger = logger or self._default_logger
+
+        if not os.path.exists(backup_folder):
+            logger.warning(f"Backup folder '{backup_folder}' does not "
+                           f"exist... Creating folder '{backup_folder}'")
+            os.makedirs(backup_folder)
+
+        filename = os.path.basename(file_path)
+        backup_file_path = os.path.join(backup_folder,
+                                        f"{filename}.bak")
+
+        try:
+            shutil.copyfile(file_path, backup_file_path)
+            logger.info(f"Backup created: {backup_file_path}")
+        except Exception as e:
+            logger.error(f"Error creating backup: {e}")
+            raise e
+
+    def clear_json(self, file_path: str,
+                   logger: logging.Logger = None,
+                   backup: bool = True):
+        if backup:
+            self.backup_json(file_path, logger=logger)
+
+        with open(file_path, "w") as file:
+            json.dump({}, file)
+
+        logger = logger or self._default_logger
+        logger.info(f"{os.path.basename(file_path)} cleared/reset.")
