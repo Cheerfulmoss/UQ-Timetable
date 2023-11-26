956e4bb5d62d45dbbe2b744a042fdd46b477306e
Alexander Bürow <41566305+Cheerfulmoss@users.noreply.github.com>
2023-11-26 16:58:12 +1000

    cogs.py: Added `CACHE_MAX_SIZE`

--- ttable-d-bot/constants/cogs.py
+++ ttable-d-bot/constants/cogs.py
@@ -2,5 +2,6 @@
 BASE_FILES_DIR = "base-files"
 API_CACHE_NAME = "api-calls-cache.json"
 ADMIN_STORE_NAME = "admin.json"
+CACHE_MAX_SIZE = 1.5  # giga bytes
 
 ALLOWED_ACCOUNTS = [398437778788188163]
