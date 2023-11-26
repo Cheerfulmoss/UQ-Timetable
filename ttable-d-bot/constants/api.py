1d06dd9ea819ed03e80d626d2b7f281eba1cc4d8
Alexander Bürow <41566305+Cheerfulmoss@users.noreply.github.com>
2023-11-26 18:37:13 +1000

    api.py: Reverted addition of `API_CALL_TIME_WARN`, wrong file :)

--- ttable-d-bot/constants/api.py
+++ ttable-d-bot/constants/api.py
@@ -1,4 +1,2 @@
 TIMETABLE_API_URL = "https://timetable.my.uq.edu.au/odd/rest/timetable/subjects"
 DATETIME_FORMAT = "%H:%M"
-API_CALL_TIME_WARN = 5000  # Time in ms for the console to warn about an API
-                           # call taking too long
