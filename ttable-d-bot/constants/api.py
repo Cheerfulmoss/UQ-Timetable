0f34be99f4927bb101cba2350880cc2ed86a53a3
Alexander Bürow <41566305+Cheerfulmoss@users.noreply.github.com>
2023-11-27 02:33:29 +1000

    api.py: Added `KEY_MAPPINGS`

--- ttable-d-bot/constants/api.py
+++ ttable-d-bot/constants/api.py
@@ -1,2 +1,10 @@
 TIMETABLE_API_URL = "https://timetable.my.uq.edu.au/odd/rest/timetable/subjects"
 DATETIME_FORMAT = "%H:%M"
+KEY_MAPPINGS = {
+    "activity_type": "activity",
+    "day_of_week": "day",
+    "activitiesDays": "schedule",
+    "color": "colour",
+    "selectable": "is-open",
+    "availability": "spots",
+}
