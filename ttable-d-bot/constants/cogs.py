3723b59c169359a4b36bdb4e21f00b34e47dd11c
Alexander Bürow <41566305+Cheerfulmoss@users.noreply.github.com>
2023-11-26 18:37:43 +1000

    cogs.py: Added `API_CALL_TIME_WARN`. correct file this time :)

--- ttable-d-bot/constants/cogs.py
+++ ttable-d-bot/constants/cogs.py
@@ -5,4 +5,7 @@ CACHE_MAX_SIZE = 1.5  # giga bytes
 CACHE_MAX_SIZE_MULT = 10  # If the cache is bigger than CACHE_MAX_SIZE *
                           # CACHE_MAX_SIZE_MULT no backup is saved.
 
+API_CALL_TIME_WARN = 5000  # Time in ms for the console to warn about an API
+                           # call taking too long
+
 ALLOWED_ACCOUNTS = [398437778788188163]
