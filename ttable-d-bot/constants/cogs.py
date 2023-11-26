7b86cb1b3c5a175232aa4246edb79878c7419f85
Alexander Bürow <41566305+Cheerfulmoss@users.noreply.github.com>
2023-11-26 18:10:03 +1000

    cogs.py: Added `CACHE_MAX_SIZE_MULT`

--- ttable-d-bot/constants/cogs.py
+++ ttable-d-bot/constants/cogs.py
@@ -1,7 +1,8 @@
-
 BASE_FILES_DIR = "base-files"
 API_CACHE_NAME = "api-calls-cache.json"
 ADMIN_STORE_NAME = "admin.json"
 CACHE_MAX_SIZE = 1.5  # giga bytes
+CACHE_MAX_SIZE_MULT = 10  # If the cache is bigger than CACHE_MAX_SIZE *
+                          # CACHE_MAX_SIZE_MULT no backup is saved.
 
 ALLOWED_ACCOUNTS = [398437778788188163]
